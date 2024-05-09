9a23c167c31bae59365b3672ce201f2e6f6fd273
Akash Yadav <150064374+Akash4317@users.noreply.github.com>
2024-05-09 13:44:47 +0530

    now navigating to home

--- Client/src/Routes/AllRoutes.tsx
+++ Client/src/Routes/AllRoutes.tsx
@@ -1,16 +1,22 @@
 
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from '../Pages/Home'
 import Login from '../Pages/Login'
 import Register from '../Pages/Register'
 import PrivateRoute from './PrivateRoute'
 import Admin from '../Pages/Admin'
+import { useContext } from 'react'
+import { AuthContext } from '../Context/AuthContextProvider'
 
 const AllRoutes = () => {
+    const { auth } = useContext(AuthContext);
     return (
         <div>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route
+                    path="/"
+                    element={auth.isAuth ? <Navigate to="/home" /> : <Login />}
+                />
                 <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
                 <Route path="/admin" element={<PrivateRoute><Admin /></PrivateRoute>} />
                 <Route path="/register" element={<Register />} />
